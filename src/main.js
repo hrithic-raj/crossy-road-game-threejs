1ceb77c73ba4d164ff91c5c72825621bb861e071
Hrithic <hrithicraj730@gmail.com>
2025-06-23 14:41:26 +0530

    Player.js created, main updated

--- src/main.js
+++ src/main.js
@@ -7,10 +7,15 @@ import { player } from "./components/Player";
 const scene = new THREE.Scene();
 scene.add(player);
 
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
+
+const dirLight = new THREE.DirectionalLight();
+dirLight.position.set(-100, -100, 200);
+scene.add(dirLight);
+
 const camera = Camera();
 scene.add(camera);
 
 const renderer = Renderer();
 renderer.render(scene, camera);
-
-
